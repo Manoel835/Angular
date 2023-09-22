0c01a2c2fe353fbbc8ab6c28ba6600a85a536d9b
Manoel835 <mftneto@gmail.com>
2023-09-22 15:48:03 -0300

    Interpolação

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+})
+export class FirstComponentComponent {
+  name: string = 'Manoel Felipe';
+  age: number = 20;
+  class: number = 6;
+}
