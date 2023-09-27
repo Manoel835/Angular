6d3d2b0da66733a532c990cd2f8ebea2e555639b
Manoel835 <mftneto@gmail.com>
2023-09-27 15:07:01 -0300

    Componente Pai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  useName = 'MANOLO';
+  subName = 'FELIPE';
+  Data = {
+    email: 'mftneto@gmail.com',
+    Altura: 1.81,
+    Peso: 78,
+  };
   title = 'curso-angular';
 }
